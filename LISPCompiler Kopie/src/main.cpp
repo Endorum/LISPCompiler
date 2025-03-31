5c17d5cab80c6eab45573e4012024f628c877035
Endorum <79927600+Endorum@users.noreply.github.com>
2025-03-31 02:21:29 +0200

    i dont even know

---
+++ LISPCompiler Kopie/src/main.cpp
@@ -0,0 +1,22 @@
+#include "../include/tokenizer.hpp"
+#include "../include/parser.hpp"
+
+
+int main(){
+    std::string input = "(+ 1 2)";
+
+    Tokenizer tokenizer;
+    tokenizer.setInput(input);
+    tokenizer.tokenize();
+
+    tokenizer.printTokens();
+
+
+    std::vector<Token> tokens = tokenizer.getTokens();
+    
+    Parser parser(tokens);
+
+    parser.parse();
+
+
+}
